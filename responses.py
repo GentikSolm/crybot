38cafacbeb732e232ee1d98ef30cabc6b4fdeb39
Alm282 <81062483+Alm282@users.noreply.github.com>
2022-12-25 21:11:46 -0500

    Feat: Add additional responses

--- responses.py
+++ responses.py
@@ -5,13 +5,17 @@ def ran_index(phrases: list):
 lost_your_streak = [
     "Seems like you lost your streak! New one starts.... Now!",
     "I mean at least you stopped crying for a day or two...",
-    "Welcome back to the club champ. We missed you"
+    "Welcome back to the club champ. We missed you",
+    "You lost your streak. No need to cry about it though"
     ]
 
 increase_streak = [
     "You're on a roll",
     "I see you're crying in the club again",
-    "Another day another tear"
+    "Another day another tear",
+    "Right on time",
+    "Do you have an alarm set for this?",
+    "And how does that make you feel?"
     ]
 
 increase_total = [
@@ -21,12 +25,18 @@ increase_total = [
     "I don't get paid enough to keep track of this shit",
     "Do me a favor, go look in the mirror and say 'HES HIM' and point to yourself sternly",
     "Are you trying to make a lake or something here?",
-    "Someone PLEASE get this bruh a tissue over here, ***ASAP***"
+    "Someone PLEASE get this bruh a tissue over here, ***ASAP***",
+    "The sea level grew by a small fraction",
+    "Me too",
+    "I wasn't expecting you until tomorrow!",
+    "Back so soon?"
     ]
 
 new_crier = [
     "A new guy huh? Welcome fam",
     "Seems like you're new here, your crying streak starts.... Now!",
-    "I'm just here to shame you don't worry. I'll be keeping an eye on you"
+    "I'm just here to shame you don't worry. I'll be keeping an eye on you",
+    "Soon you will know the ways of crying frequently",
+    "I'm sorry that you are starting to cry with us. Hold on for the ride"
     ]
 
